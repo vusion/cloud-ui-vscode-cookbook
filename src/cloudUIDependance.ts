703db007b85f5bd3c63580739b637d6ddd147f16
汪韬 <452663934@qq.com>
2019-05-06 19:03:18 +0800

    :art: 增加图标和说明

--- src/cloudUIDependance.ts
+++ src/cloudUIDependance.ts
@@ -1,7 +1,27 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
+function readFirstLine(path: string): Thenable<string>{
+    return new Promise((resolve, reject) => {
+        const rs = fs.createReadStream(path, {encoding: 'utf8'});
+        let acc = '';
+        let pos = 0;
+        let index = 0;
+        rs
+            .on('data', function (chunk) {
+                index = chunk.indexOf('\n');
+                acc += chunk;
+                index !== -1 ? rs.close() : pos += chunk.length;
+            })
+            .on('close', function () {
+                resolve(acc.slice(0, pos + index));
+            })
+            .on('error', function (err) {
+                reject(err);
+            })
+    })
 
+}
 export class CloudUIProvider implements vscode.TreeDataProvider<CloudUI> {
 
 	private _onDidChangeTreeData: vscode.EventEmitter<CloudUI | undefined> = new vscode.EventEmitter<CloudUI | undefined>();
@@ -26,7 +46,7 @@ export class CloudUIProvider implements vscode.TreeDataProvider<CloudUI> {
 
         const cloudUIPath = path.resolve(this.workspaceRoot, 'node_modules/cloud-ui.vusion/src');
         if (this.pathExists(cloudUIPath)) {
-            return Promise.resolve(this.getCloudUIInDependance(cloudUIPath));
+            return this.getCloudUIInDependance(cloudUIPath);
         } else {
             vscode.window.showInformationMessage('cloud-ui.vusion');
             return Promise.resolve([]);
@@ -35,19 +55,26 @@ export class CloudUIProvider implements vscode.TreeDataProvider<CloudUI> {
 
     }
 
-    private getCloudUIInDependance(cloudUIPath: string): CloudUI[]{
+    private getCloudUIInDependance(cloudUIPath: string): Thenable<CloudUI[]>{
         const dir = fs.readdirSync(cloudUIPath);
         // maybe can read MD file
-        return dir.filter(component => {
+        return Promise.all(dir.filter(component => {
             if(!component.endsWith('.vue'))
                 return false;
             const dir = fs.readdirSync(path.join(cloudUIPath, component));
             return dir.indexOf('README.md') !== -1;
-        }).map(component => new CloudUI(component, 'xxx', vscode.TreeItemCollapsibleState.None, {
-            command: 'extension.openUIMarkDown',
-            title: '',
-            arguments: [component, path.join(cloudUIPath, component)]
-        }))
+        }).map(component => {
+            const targetPath = path.join(cloudUIPath, component);
+            const READMEPath = path.join(targetPath, 'README.md');
+            // const data = fs.readFileSync(READMEPath);
+            return readFirstLine(READMEPath).then(data => {
+                return new CloudUI(component, data, vscode.TreeItemCollapsibleState.None, {
+                    command: 'extension.openUIMarkDown',
+                    title: '',
+                    arguments: [component, targetPath]
+                })
+            })
+        }));
     }
 
 	private pathExists(p: string): boolean {
